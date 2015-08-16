e50e6e120d74428baf8ffdc04502efbadc9154b2
LuisToscano <luisgtoscano@gmail.com>
2015-08-16 15:56:36 -0500

    Update timerUtilities.js

--- js/timerUtilities.js
+++ js/timerUtilities.js
@@ -4,50 +4,78 @@
  * and open the template in the editor.
  */
 
-function inicializarTimer(sym, nombreTimer) {
-    var timerObj = sym.$(nombreTimer);
-    $.getJSON("config.json", function (data) {
+$("body").on("TimeAlert", function (data) {
+    cambiarEstadoTimer(data.sym, data.tag);
+});
+
+function cambiarEstadoTimer(sym, estado) {
+    var stage = $(sym.getComposition().getStage().ele);
+    var timerObj = buscar_sym(sym, stage.prop("timer"), true);
+
+    if (timerObj.prop("alertState") !== ("" + estado)) {
+        buscar_sym(sym, stage.prop("timer")).play(estado);
+        timerObj.prop("alertState", "" + estado);
+    }
+}
+
+//*************************************************************************************
+
+function inicializarTimer(sym) {
+    var stage = $(sym.getComposition().getStage().ele);
+
+
+    $.getJSON("timer_config.json").done(function (data) {
+        var timerObj = buscar_sym(sym, data.sym, true);
         $.each(data, function (key, val) {
             timerObj.prop(key, val);
         });
-    }).done(function () {
-        var spanElement = timerObj.find("p span");
-        spanElement.html(secondsToClockFormat(timerObj.prop("segundos")));
-        timerObj.prop("segundos_restantes", timerObj.prop("segundos"));
+
+        stage.prop("timer", data.sym);
+        var spanElement = timerObj.find("p");
         timerObj.prop("timer_text", spanElement);
+        setHTMLTimer(timerObj.prop("segundos"), sym);
+        timerObj.prop("segundos_restantes", timerObj.prop("segundos"));
         timerObj.prop("interval_id", -1);
-        timerObj.prop("nombre", nombreTimer);
+        timerObj.prop("nombre", data.sym);
+        timerObj.prop("stopped", false);
+        timerObj.prop("alertState", "normal");
     });
 }
 
 //*************************************************************************************
 
-function startTimer(timerObj, sym) {
-    if (timerObj.prop("interval_id")<0)
+function startTimer(sym) {
+    var stage = $(sym.getComposition().getStage().ele);
+    var timerObj = buscar_sym(sym, stage.prop("timer"), true);
+    //console.log(timerObj);
+    if (timerObj.prop("interval_id") < 0 && !timerObj.prop("stopped"))
     {
         var interval_id = setInterval(function () {
             var currentTime = timerObj.prop("segundos_restantes");
             currentTime--;
             timerObj.prop("timer_text").html(secondsToClockFormat(currentTime));
             timerObj.prop("segundos_restantes", currentTime);
-            
-            if($.inArray(currentTime, timerObj.prop("alertas"))>=0){
+            var strSegundos = ""+timerObj.prop("segundos_restantes");
+            var alertas = timerObj.prop("alertas");
+
+            if (alertas.hasOwnProperty(strSegundos)) {
                 $("body").trigger({
-			type: "TimeAlert",
-                        remaining_time: currentTime, 
-                        timer_obj: timerObj,
-                        sym: sym
-		});
+                    type: "TimeAlert",
+                    remaining_time: currentTime,
+                    tag: alertas[strSegundos].tag,
+                    timerObj: timerObj,
+                    sym: sym
+                });
             }
-            
+
             if (currentTime <= 0) {
-                stopTimer(timerObj);
+                stopTimer(sym);
                 $("body").trigger({
-			type: "TimeOut",
-                        sym: sym
-		});
+                    type: "TimeOut",
+                    sym: sym
+                });
             }
-            
+
         }, 1000);
         timerObj.prop("interval_id", interval_id);
     }
@@ -55,11 +83,43 @@ function startTimer(timerObj, sym) {
 
 //*************************************************************************************
 
-function stopTimer(timerObj) {
-    if(timerObj.prop("interval_id")>=0){
+function stopTimer(sym) {
+    var stage = $(sym.getComposition().getStage().ele);
+    var timerObj = buscar_sym(sym, stage.prop("timer"), true);
+    if (timerObj.prop("interval_id") >= 0) {
+        clearInterval(timerObj.prop("interval_id"));
+        timerObj.prop("interval_id", -1);
+        timerObj.prop("stopped", true);
+    }
+}
+
+//*************************************************************************************
+
+function resetTimer(sym) {
+    var stage = $(sym.getComposition().getStage().ele);
+    var timerObj = buscar_sym(sym, stage.prop("timer"), true);
+    if (timerObj.prop("interval_id") >= 0) {
         clearInterval(timerObj.prop("interval_id"));
         timerObj.prop("interval_id", -1);
     }
+
+    var spanElement = timerObj.find("p");
+    var newSpan = $('<span />').attr('className', 'folder_name').html(secondsToClockFormat(timerObj.prop("segundos")));
+    spanElement.html(newSpan);
+    timerObj.prop("segundos_restantes", timerObj.prop("segundos"));
+    timerObj.prop("timer_text", spanElement);
+    timerObj.prop("stopped", false);
+
+    cambiarEstadoTimer(sym, "normal");
+}
+
+//*************************************************************************************
+
+function setHTMLTimer(seconds, sym) {
+    var stage = $(sym.getComposition().getStage().ele);
+    var newSpan = $('<span />').html(secondsToClockFormat(seconds));
+    var timerObj = buscar_sym(sym, stage.prop("timer"), true);
+    timerObj.prop("timer_text").html(newSpan);
 }
 
 //*************************************************************************************
@@ -87,13 +147,5 @@ function secondsToClockFormat(sec_num) {
     return time;
 }
 
-//*************************************************************************************
+//***********************************************************************************
 
-$("body").on("TimeOut", function (data) {
-   enviarEventoInteraccion("","","","","incorrect",0,2,true, data.sym);
-});
-
-$("body").on("TimeAlert", function (data) {
-    var nomb = data.timer_obj.prop("nombre");
-    data.sym.getSymbol(nomb).play(data.remaining_time);
-});
